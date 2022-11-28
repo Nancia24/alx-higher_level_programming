f16d0bebcc7544b94795930939a07d9e7c4694e1
root <nanciamwaramba@gmail.com>
2022-11-28 19:32:24 +0000

    mandatory

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {:s}!".format(str1))
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {:s}!".format(str1))
